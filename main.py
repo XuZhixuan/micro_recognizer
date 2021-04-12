3ee6e3d54c9bdc2743400fe3751dfdde206326cf
XuZhixuan <sxxuzhixuan@hotmail.com>
2021-04-12 08:45:46 +0800

    自动将图像 ndarray 转换为 Tensor-GPU

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import Modules
 import Tools
 
 import numpy
+import torch
 
 from helper import *
 
@@ -12,6 +13,7 @@ class Program:
         self.validate_set = None
         self.data = Tools.ImageLoader.loads('images/load_list.txt')
         self.model = Modules.Network()
+        self.loss_function = torch.nn.CrossEntropyLoss
 
     def create_dataset(self):
         """ Create the dataset for training & validation
@@ -20,7 +22,8 @@ class Program:
         gray_images = []
         scale = []
         for image in self.data:
-            gray_images.append(numpy.array(image.grayscale))
+            tensor = torch.from_numpy(numpy.array(image.grayscale))
+            gray_images.append(tensor.cuda())
             scale.append(image.thermal)
 
         x_train, x_validate, y_train, y_validate = train_test_split(gray_images, scale, validate_size=0.3)
@@ -28,15 +31,19 @@ class Program:
         self.train_set = (x_train, y_train)
         self.validate_set = (x_validate, y_validate)
 
-    def train_network(self):
-        history = self.model.net.fit(
-
-        )
-        return history
+    def train_network(self, epochs):
+        for epoch in range(epochs):
+            print('Epoch: %d' % epoch)
+            train_loss = 0.
+            train_acc = 0.
+            for x, y in self.train_set[0], self.train_set[1]:
+                out = self.model.net(x)
+                loss = self.loss_function(out, y)
+                train_loss += loss
 
     def main(self):
         self.create_dataset()
-        self.train_network()
+        self.train_network(5)
         pass
 
 
